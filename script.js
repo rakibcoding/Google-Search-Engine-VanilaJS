8931a010786020c2edd5f5c66a04551691e714e7
Rakib-Coder00 <rhbappy456@gmail.com>
2023-10-18 19:25:50 +0600

    search also work for enter button

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let query = document.querySelector(".query");
 let searchBtn = document.querySelector(".searchBtn");
 
-searchBtn.onclick = function () {
-  let url = "https://www.google.com/search?q="+ query.value;
+// Function to perform the search
+const performSearch = () => {
+  let url = "https://www.google.com/search?q=" + query.value;
   window.open(url, "_self");
 };
+
+// Trigger search on button click =>
+searchBtn.onclick = performSearch;
+
+// Trigger search when Enter key is pressed in the input field
+query.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    performSearch();
+  }
+});
