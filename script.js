7d0029ace0f66098ee5158b110b2bbbf32afe5fc
Rakib Hassan <rhbappy456@gmail.com>
2024-01-27 00:36:59 +0600

    update for empty input value

--- script.js
+++ script.js
@@ -2,8 +2,42 @@ let query = document.querySelector(".query");
 let searchBtn = document.querySelector(".searchBtn");
 let feelingBtn = document.querySelector(".feelingBtn");
 
+// Array of words
+const words = [
+  "apple",
+  "banana",
+  "orange",
+  "grape",
+  "kiwi",
+  "pineapple",
+  "javascript",
+  "python",
+  "java",
+  "typescript",
+  "ruby",
+  "csharp",
+  "html",
+  "css",
+  "php",
+  "go",
+  "swift",
+  "rust",
+  "kotlin",
+];
+
+// Function to generate a random word
+function generateRandomWord() {
+  const randomIndex = Math.floor(Math.random() * words.length);
+  return words[randomIndex];
+}
+
+const randomWord = generateRandomWord();
+
 // Function to perform the search
 const performSearch = () => {
+  if (query.value === "") {
+    query.value = randomWord;
+  }
   let url = "https://www.google.com/search?q=" + query.value;
   window.open(url, "_self");
 };
