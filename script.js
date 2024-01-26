09440eaba838fa2ff0e1a99c40474030135bac21
Rakib Hassan <rhbappy456@gmail.com>
2024-01-27 00:24:41 +0600

    added feeling lucky button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let query = document.querySelector(".query");
 let searchBtn = document.querySelector(".searchBtn");
+let feelingBtn = document.querySelector(".feelingBtn");
 
 // Function to perform the search
 const performSearch = () => {
@@ -9,6 +10,8 @@ const performSearch = () => {
 
 // Trigger search on button click =>
 searchBtn.onclick = performSearch;
+
+feelingBtn.onclick = performSearch;
 
 // Trigger search when Enter key is pressed in the input field
 query.addEventListener("keyup", function (event) {
